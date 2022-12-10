f3c13cacddad0223ab703e33d78e7eb56c372613
Mohit <saraffmohit@gmail.com>
2022-12-10 17:22:25 +0530

    Add multiple notes from an array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Note";
+import Notes from "./Note";
 
 const App = () => {
     return (
         <div>
             <Header />
-            <Note />
+            <Notes />
             <Footer />
         </div>
     );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import notes from "../notes"
 
-const Note = () => {
-    return (
-        <div className="note">
-            <h1>This is the note title</h1>
-            <p>This is the note content</p>
-        </div>
-    );
-}
+const Note = (props) => (
+    <div className="note">
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
+    </div>
+);
 
-export default Note;
+const Notes = () => (
+    notes.map( note => (
+        <Note
+            key={note.key}
+            title={note.title}
+            content={note.content}
+        />
+    ))
+)
+
+export default Notes;
