97d56ed949f427d12d2165621eb6800408a7b07a
Mohit <saraffmohit@gmail.com>
2022-12-20 00:02:51 +0530

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-    const currentYear = new Date().getFullYear();
     return (
         <footer>
-            <p>&copy; {currentYear}</p>
+            <p>Made By <a href="https://github.com/MohitSaraff/Keeper" target="_blank" rel="noreferrer" title="Source Code"><b>Mohit</b></a></p>
+                <ul class="social-icon">
+                    <li><a href="https://www.linkedin.com/in/mohit-kr-saraf" target="_blank" rel="noreferrer" title="Follow me on LinkedIn"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="https://twitter.com/saraffmohit" target="_blank" rel="noreferrer" title="Follow me on Twitter"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="https://github.com/MohitSaraff" target="_blank" rel="noreferrer" title="Follow me on GitHub"><i class="fa fa-github"></i></a></li>
+                    <li><a href="mailto:saraffmohit@gmail.com" target="_blank" rel="noreferrer" title="Contact me on Mail"><i class="fa fa-envelope"></i></a></li>
+                </ul>
         </footer>
     );
 }
